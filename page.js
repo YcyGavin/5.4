2273dc5e74ef13d93da3b460146c9b1e7904a940
YcyGavin <103399490+YcyGavin@users.noreply.github.com>
2023-01-22 21:18:04 +0800

    Update page.js

--- page.js
+++ page.js
@@ -1,3 +1,4 @@
+//Realize picture jump, its role is to jump to the image, click the button to jump to the image, help users better understand our course //
 var slideIndex = 1;
 showDivs(slideIndex);
 
@@ -16,6 +17,7 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";  
 }
 
+//Form validation, the purpose of which is to verify that the information entered by the user complies with JavaScript regulations, this HTML form will trigger this action="mailto:username@hotmail.com" //
 function validateform(){  
   var name=document.myform.name.value;  
   var password=document.myform.password.value; 
@@ -40,26 +42,33 @@ function validateform(){
     }  
   }
   
+  //Make link jumps //
+  //Jump to the introduction of the Financial Management Section //
   function myFunction() {
     location.replace("https://www.netsuite.com/portal/resource/articles/financial-management/financial-management.shtml")
   }
 
+  //Jump to the introduction of the Advanced mathematics course//
   function myFunction2() {
     location.replace("https://pll.harvard.edu/subject/mathematics")
   }
 
+  //Jump to the introduction of the Venture Capital Course//
   function myFunction3() {
     location.replace("https://executive.berkeley.edu/venture-capital/executive")
   }
 
+  //Jump to the introduction of the Real Estate Investment Course//
   function myFunction4() {
     location.replace("https://extension.harvard.edu/academics/programs/real-estate-investment-certificate/#:~:text=%20The%20professional%20graduate%20certificate%20in%20Real%20Estate,%28choose%20one%20course%20from%20select%20group%29%20More%20")
   }
 
+  //Jump to the introduction of the Internet Finance Courses//
   function myFunction5() {
     location.replace("https://www.coursera.org/browse/business/finance")
   }
 
+  //Jump to the introduction of the Internet Big Data course//
   function myFunction6() {
     location.replace("https://www.edx.org/learn/big-data")
   }
